aee73c3c613306647812ee9a60f642538a0de894
Hafid Afridian <afridianhafid@gmail.com>
2025-09-05 19:56:23 +0700

    add model and migration for school year.

---
+++ database/migrations/2025_09_05_124849_create_school_years_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('school_years', function (Blueprint $table) {
+            $table->id();
+            $table->integer('year_start');
+            $table->integer('year_end');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('school_years');
+    }
+};
